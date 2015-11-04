63a3bdd4a30d127230a0ab5dd0c783d54c62c4be
Pavel Richter <pavel.richter@tul.cz>
2015-11-04 08:10:29 +0100

    js - test

--- src/JobsScheduler/communication/jobs_communicator.py
+++ src/JobsScheduler/communication/jobs_communicator.py
@@ -25,9 +25,10 @@ class JobsCommunicator(Communicator):
         
     def  standart_action_function_before(self, message):
         """before action function"""
-        if message.action_type == tdata.ActionType.installation:
+        if message.action_type == tdata.ActionType.installation:            
             resent, mess = super(JobsCommunicator, self).standart_action_function_before(message)
             if self.is_installed():
+                logging.debug("Job application began start")
                 action = tdata.Action(tdata.ActionType.ok)
                 return False, action.get_message()
             return resent, mess
