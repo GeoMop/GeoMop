b9a13e8b4a83879b46778677189610f34cf61cb8
Jan Gabriel <jan.gabriel@tul.cz>
2015-11-05 14:10:48 +0100

    Environment presets working with data fields and saving.

--- src/JobsScheduler/ui/dialogs/env_dialog.py
+++ src/JobsScheduler/ui/dialogs/env_dialog.py
@@ -7,6 +7,7 @@ Env dialog
 
 from PyQt5 import QtGui
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
 
 from ui.dialogs.dialogs import UiFormDialog, AFormDialog
 
@@ -52,19 +53,68 @@ class EnvDialog(AFormDialog):
         # connect generic presets slots (must be called after UI setup)
         super(EnvDialog, self)._connect_slots()
         # specific slots
+        self.ui.sclEnableCheckBox.stateChanged.connect(
+            lambda state: self.ui.sclEnableLineEdit.setDisabled(not state)
+        )
+        self.ui.addModuleCheckBox.stateChanged.connect(
+            lambda state: self.ui.addModuleLineEdit.setDisabled(not state)
+        )
+        self.ui.mpiSclEnableCheckBox.stateChanged.connect(
+            lambda state: self.ui.mpiSclEnableLineEdit.setDisabled(not
+                                                                   state)
+        )
+        self.ui.mpiAddModuleCheckBox.stateChanged.connect(
+            lambda state: self.ui.mpiAddModuleLineEdit.setDisabled(not state)
+        )
 
     def get_data(self):
         data = (self.ui.idLineEdit.text(),
-                self.ui.nameLineEdit.text())
+                self.ui.nameLineEdit.text(),
+                "description",
+                self.ui.pythonExecLineEdit.text(),
+                self.ui.sclEnableLineEdit.text()
+                if self.ui.sclEnableCheckBox.isChecked() else None,
+                self.ui.addModuleLineEdit.text()
+                if self.ui.addModuleCheckBox.isChecked() else None,
+                self.ui.mpiSclEnableLineEdit.text()
+                if self.ui.mpiSclEnableCheckBox.isChecked() else None,
+                self.ui.mpiAddModuleLineEdit.text()
+                if self.ui.mpiAddModuleCheckBox.isChecked() else None,
+                self.ui.mpiccPathLineEdit.text())
         return data
 
     def set_data(self, data=None):
         if data:
             self.ui.idLineEdit.setText(data[0])
             self.ui.nameLineEdit.setText(data[1])
+            self.ui.pythonExecLineEdit.setText(data[3])
+            if data[4]:
+                self.ui.sclEnableCheckBox.setCheckState(Qt.Checked)
+                self.ui.sclEnableLineEdit.setText(data[4])
+            if data[5]:
+                self.ui.addModuleCheckBox.setCheckState(Qt.Checked)
+                self.ui.addModuleLineEdit.setText(data[5])
+            if data[6]:
+                self.ui.mpiSclEnableCheckBox.setCheckState(Qt.Checked)
+                self.ui.mpiSclEnableLineEdit.setText(data[6])
+            if data[7]:
+                self.ui.mpiAddModuleCheckBox.setCheckState(Qt.Checked)
+                self.ui.mpiAddModuleLineEdit.setText(data[7])
+            self.ui.mpiccPathLineEdit.setText(data[8])
+
         else:
             self.ui.idLineEdit.clear()
             self.ui.nameLineEdit.clear()
+            self.ui.pythonExecLineEdit.clear()
+            self.ui.sclEnableCheckBox.setCheckState(Qt.Unchecked)
+            self.ui.sclEnableLineEdit.clear()
+            self.ui.addModuleCheckBox.setCheckState(Qt.Unchecked)
+            self.ui.addModuleLineEdit.clear()
+            self.ui.mpiSclEnableCheckBox.setCheckState(Qt.Unchecked)
+            self.ui.mpiSclEnableLineEdit.clear()
+            self.ui.mpiAddModuleCheckBox.setCheckState(Qt.Unchecked)
+            self.ui.mpiAddModuleLineEdit.clear()
+            self.ui.mpiccPathLineEdit.clear()
 
 
 class UiEnvDialog(UiFormDialog):
@@ -134,6 +184,65 @@ class UiEnvDialog(UiFormDialog):
         self.formLayout2.setContentsMargins(0, 5, 0, 5)
         self.mainVerticalLayout.addLayout(self.formLayout2)
 
+        # 1 row
+        self.pythonExecLabel = QtWidgets.QLabel(self.mainVerticalLayoutWidget)
+        self.pythonExecLabel.setObjectName("pythonExecLabel")
+        self.pythonExecLabel.setText("Interpreter:")
+        self.formLayout2.setWidget(1, QtWidgets.QFormLayout.LabelRole,
+                                   self.pythonExecLabel)
+        self.pythonExecLineEdit = QtWidgets.QLineEdit(
+            self.mainVerticalLayoutWidget)
+        self.pythonExecLineEdit.setObjectName("pythonExecLineEdit")
+        self.pythonExecLineEdit.setPlaceholderText("for example: python3")
+        self.pythonExecLineEdit.setProperty("clearButtonEnabled", True)
+        self.formLayout2.setWidget(1, QtWidgets.QFormLayout.FieldRole,
+                                   self.pythonExecLineEdit)
+
+        # 2 row
+        self.sclEnableLabel = QtWidgets.QLabel(self.mainVerticalLayoutWidget)
+        self.sclEnableLabel.setObjectName("sclEnableLabel")
+        self.sclEnableLabel.setText("SCL Enable:")
+        self.formLayout2.setWidget(2, QtWidgets.QFormLayout.LabelRole,
+                                   self.sclEnableLabel)
+        self.sclEnableRowSplit = QtWidgets.QHBoxLayout()
+        self.sclEnableRowSplit.setObjectName("sclEnableRowSplit")
+        self.sclEnableLineEdit = QtWidgets.QLineEdit(
+            self.mainVerticalLayoutWidget)
+        self.sclEnableLineEdit.setObjectName("sclEnableLineEdit")
+        self.sclEnableLineEdit.setPlaceholderText("for example: python33")
+        self.sclEnableLineEdit.setProperty("clearButtonEnabled", True)
+        self.sclEnableLineEdit.setDisabled(True)
+        self.sclEnableCheckBox = QtWidgets.QCheckBox(
+            self.mainVerticalLayoutWidget)
+        self.sclEnableCheckBox.setObjectName("sclEnableCheckBox")
+        self.sclEnableRowSplit.addWidget(self.sclEnableCheckBox)
+        self.sclEnableRowSplit.addWidget(self.sclEnableLineEdit)
+        self.formLayout2.setLayout(2, QtWidgets.QFormLayout.FieldRole,
+                                   self.sclEnableRowSplit)
+
+        # 3 row
+        self.addModuleLabel = QtWidgets.QLabel(self.mainVerticalLayoutWidget)
+        self.addModuleLabel.setObjectName("addModuleLabel")
+        self.addModuleLabel.setText("Add module:")
+        self.formLayout2.setWidget(3, QtWidgets.QFormLayout.LabelRole,
+                                   self.addModuleLabel)
+        self.addModuleRowSplit = QtWidgets.QHBoxLayout()
+        self.addModuleRowSplit.setObjectName("addModuleRowSplit")
+        self.addModuleLineEdit = QtWidgets.QLineEdit(
+            self.mainVerticalLayoutWidget)
+        self.addModuleLineEdit.setObjectName("addModuleLineEdit")
+        self.addModuleLineEdit.setPlaceholderText("for example: "
+                                                  "python34-module-gcc")
+        self.addModuleLineEdit.setProperty("clearButtonEnabled", True)
+        self.addModuleLineEdit.setDisabled(True)
+        self.addModuleCheckBox = QtWidgets.QCheckBox(
+            self.mainVerticalLayoutWidget)
+        self.addModuleCheckBox.setObjectName("addModuleCheckBox")
+        self.addModuleRowSplit.addWidget(self.addModuleCheckBox)
+        self.addModuleRowSplit.addWidget(self.addModuleLineEdit)
+        self.formLayout2.setLayout(3, QtWidgets.QFormLayout.FieldRole,
+                                   self.addModuleRowSplit)
+
         # divider
         self.formDivider1 = QtWidgets.QFrame(self.mainVerticalLayoutWidget)
         self.formDivider1.setObjectName("formDivider")
@@ -155,5 +264,67 @@ class UiEnvDialog(UiFormDialog):
         self.formLayout3.setContentsMargins(0, 5, 0, 5)
         self.mainVerticalLayout.addLayout(self.formLayout3)
 
+        # 1 row
+        self.mpiSclEnableLabel = QtWidgets.QLabel(
+            self.mainVerticalLayoutWidget)
+        self.mpiSclEnableLabel.setObjectName("mpiSclEnableLabel")
+        self.mpiSclEnableLabel.setText("SCL Enable:")
+        self.formLayout3.setWidget(1, QtWidgets.QFormLayout.LabelRole,
+                                   self.mpiSclEnableLabel)
+        self.mpiSclEnableRowSplit = QtWidgets.QHBoxLayout()
+        self.mpiSclEnableRowSplit.setObjectName("mpiSclEnableRowSplit")
+        self.mpiSclEnableLineEdit = QtWidgets.QLineEdit(
+            self.mainVerticalLayoutWidget)
+        self.mpiSclEnableLineEdit.setObjectName("mpiSclEnableLineEdit")
+        self.mpiSclEnableLineEdit.setPlaceholderText("for example: "
+                                                     "rock-openmpi")
+        self.mpiSclEnableLineEdit.setProperty("clearButtonEnabled", True)
+        self.mpiSclEnableLineEdit.setDisabled(True)
+        self.mpiSclEnableCheckBox = QtWidgets.QCheckBox(
+            self.mainVerticalLayoutWidget)
+        self.mpiSclEnableCheckBox.setObjectName("mpiSclEnableCheckBox")
+        self.mpiSclEnableRowSplit.addWidget(self.mpiSclEnableCheckBox)
+        self.mpiSclEnableRowSplit.addWidget(self.mpiSclEnableLineEdit)
+        self.formLayout3.setLayout(1, QtWidgets.QFormLayout.FieldRole,
+                                   self.mpiSclEnableRowSplit)
+
+        # 2 row
+        self.mpiAddModuleLabel = QtWidgets.QLabel(
+            self.mainVerticalLayoutWidget)
+        self.mpiAddModuleLabel.setObjectName("mpiAddModuleLabel")
+        self.mpiAddModuleLabel.setText("Add module:")
+        self.formLayout3.setWidget(2, QtWidgets.QFormLayout.LabelRole,
+                                   self.mpiAddModuleLabel)
+        self.mpiAddModuleRowSplit = QtWidgets.QHBoxLayout()
+        self.mpiAddModuleRowSplit.setObjectName("mpiAddModuleRowSplit")
+        self.mpiAddModuleLineEdit = QtWidgets.QLineEdit(
+            self.mainVerticalLayoutWidget)
+        self.mpiAddModuleLineEdit.setObjectName("mpiAddModuleLineEdit")
+        self.mpiAddModuleLineEdit.setPlaceholderText("for example: "
+                                                     "rock-openmpi")
+        self.mpiAddModuleLineEdit.setProperty("clearButtonEnabled", True)
+        self.mpiAddModuleLineEdit.setDisabled(True)
+        self.mpiAddModuleCheckBox = QtWidgets.QCheckBox(
+            self.mainVerticalLayoutWidget)
+        self.mpiAddModuleCheckBox.setObjectName("mpiAddModuleCheckBox")
+        self.mpiAddModuleRowSplit.addWidget(self.mpiAddModuleCheckBox)
+        self.mpiAddModuleRowSplit.addWidget(self.mpiAddModuleLineEdit)
+        self.formLayout3.setLayout(2, QtWidgets.QFormLayout.FieldRole,
+                                   self.mpiAddModuleRowSplit)
+
+        # 3 row
+        self.mpiccPathLabel = QtWidgets.QLabel(self.mainVerticalLayoutWidget)
+        self.mpiccPathLabel.setObjectName("mpiccPathLabel")
+        self.mpiccPathLabel.setText("Mpicc path:")
+        self.formLayout3.setWidget(3, QtWidgets.QFormLayout.LabelRole,
+                                   self.mpiccPathLabel)
+        self.mpiccPathLineEdit = QtWidgets.QLineEdit(
+            self.mainVerticalLayoutWidget)
+        self.mpiccPathLineEdit.setObjectName("mpiccPathLineEdit")
+        self.mpiccPathLineEdit.setPlaceholderText("alternative path to mpicc")
+        self.mpiccPathLineEdit.setProperty("clearButtonEnabled", True)
+        self.formLayout3.setWidget(3, QtWidgets.QFormLayout.FieldRole,
+                                   self.mpiccPathLineEdit)
+        
         # add button box
         self.mainVerticalLayout.addWidget(self.buttonBox)
