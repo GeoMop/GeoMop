16cffc0977176844b8c3ac730bbdc435f140ca36
Tomas Krizek <tomas.krizek@mailbox.org>
2016-05-20 01:01:58 +0200

    docker + jenkins

---
+++ docker/jenkins.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "************ RUNNING TESTS *************"
+
+# test
+docker run \
+  -v /var/lib/jenkins/workspace/GeoMop:/mnt/GeoMop \
+  geomop/test \
+  /home/geomop/test.sh
+if [[ $? != 0 ]]; then exit 1; fi
+
+echo "************ RUNNING BUILD *************"
+
+# build
+docker run \
+  -v /var/lib/jenkins/workspace/GeoMop:/mnt/GeoMop \
+  -e VERSION=$VERSION \
+  geomop/build \
+  bin/bash -c " \
+    /home/geomop/build.sh; \
+    chown -R $(id -u):$(id -g) /mnt/GeoMop \
+  "
+if [[ $? != 0 ]]; then exit 1; fi
+
+echo "************** SUCCESS ****************"
+
+exit 0
