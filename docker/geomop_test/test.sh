ceb06b5455f9d22b0acf51fde4bf1ea1b5e094e7
Radek Srb <radek.srb@tul.cz>
2017-01-30 16:34:01 +0100

    inst - tests

--- docker/geomop_test/test.sh
+++ docker/geomop_test/test.sh
@@ -24,7 +24,7 @@ DISPLAY=:1 py.test
 if [[ $? != 0 ]]; then kill $PID; exit 1; fi
 
 cd $TEST_DIR/JobPanel
-export PYTHONPATH=$SRC_DIR/JobPanel:$SRC_DIR/JobPanel/twoparty/pexpect:/$SRC_DIR/common:./mock
+export PYTHONPATH=$SRC_DIR/JobPanel:$SRC_DIR/JobPanel/twoparty/pexpect:/$SRC_DIR/common:$SRC_DIR/JobPanel/twoparty/pexpect:./mock
 DISPLAY=:1 py.test
 if [[ $? != 0 ]]; then kill $PID; exit 1; fi
 
