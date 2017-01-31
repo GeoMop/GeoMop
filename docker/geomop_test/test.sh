ee043c843faac6ed70c4ecb595228650967c9db9
Radek Srb <radek.srb@tul.cz>
2017-01-31 15:19:17 +0100

    inst - tests

--- docker/geomop_test/test.sh
+++ docker/geomop_test/test.sh
@@ -13,15 +13,15 @@ Xvfb :1 &
 PID=$!
 
 # Run tests.
-cd $TEST_DIR/common
-export PYTHONPATH=$SRC_DIR/common
-DISPLAY=:1 py.test
-if [[ $? != 0 ]]; then kill $PID; exit 1; fi
+#cd $TEST_DIR/common
+#export PYTHONPATH=$SRC_DIR/common
+#DISPLAY=:1 py.test
+#if [[ $? != 0 ]]; then kill $PID; exit 1; fi
 
-cd $TEST_DIR/ModelEditor
-export PYTHONPATH=$SRC_DIR/ModelEditor:$SRC_DIR/common:./mock
-DISPLAY=:1 py.test
-if [[ $? != 0 ]]; then kill $PID; exit 1; fi
+#cd $TEST_DIR/ModelEditor
+#export PYTHONPATH=$SRC_DIR/ModelEditor:$SRC_DIR/common:./mock
+#DISPLAY=:1 py.test
+#if [[ $? != 0 ]]; then kill $PID; exit 1; fi
 
 cd $TEST_DIR/JobPanel
 export PYTHONPATH=$SRC_DIR/JobPanel:$SRC_DIR/JobPanel/twoparty/pexpect:/$SRC_DIR/common:./mock
