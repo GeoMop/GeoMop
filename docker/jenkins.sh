798bdcd7da1b3ce5c491a480594a00bcdbe4e37d
Radek Srb <radek.srb@tul.cz>
2017-02-01 17:53:05 +0100

    inst - tests

--- docker/jenkins.sh
+++ docker/jenkins.sh
@@ -4,14 +4,15 @@ echo "************ RUNNING TESTS *************"
 
 # test
 docker run \
-  --net=host \
   -v /var/lib/jenkins/workspace/GeoMop:/mnt/GeoMop \
   -v /home/geomop/.ssh:/mnt/ssh \
+  -p 22:22 \
   geomop/test \
   /home/geomop/test.sh
 if [[ $? != 0 ]]; then exit 1; fi
 
 #-v /home/geomop/.ssh:/home/geomop/.ssh \
+#--net=host \
 
 echo "************ RUNNING BUILD *************"
 
