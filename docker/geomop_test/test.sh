5cf137066e426bbf3c54ff88358c4408e8002663
Radek Srb <radek.srb@tul.cz>
2017-02-03 12:12:30 +0100

    inst - tests

--- docker/geomop_test/test.sh
+++ docker/geomop_test/test.sh
@@ -18,7 +18,9 @@ echo test:MojeHeslo123 | chpasswd
 
 mkdir -p /var/run/sshd
 #/usr/sbin/sshd -D
-
+sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
+service ssh restart
 sleep 10
 
 # copy ssh key
