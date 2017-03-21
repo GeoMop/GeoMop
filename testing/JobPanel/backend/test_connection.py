607b606bd0d3619ad375af3ff0bed4cf30c9d109
Radek Srb <radek.srb@tul.cz>
2017-03-21 22:43:59 +0100

    jp - connection tests

---
+++ testing/JobPanel/backend/test_connection.py
@@ -0,0 +1,150 @@
+# prerequisites for this tests:
+# 1: on machine must be user "test" with ssh keys set for user which runs tests
+# 2: directory "/test/test_dir" must be writable for user which runs tests
+
+
+from backend._connection import *
+
+import threading
+import socket
+import socketserver
+import os
+import shutil
+
+
+def test_port_forwarding():
+    class Server(socketserver.ThreadingTCPServer):
+        daemon_threads = True
+        allow_reuse_address = True
+
+    class RequestHandler(socketserver.BaseRequestHandler):
+        def handle(self):
+            data = str(self.request.recv(1024), 'ascii')
+            if data == "hello":
+                self.request.sendall(bytes("hello", 'ascii'))
+
+    def connection_test(ip, port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((ip, port))
+        try:
+            sock.sendall(bytes("hello", 'ascii'))
+            response = str(sock.recv(1024), 'ascii')
+            if response == "hello":
+                return True
+        finally:
+            sock.close()
+        return False
+
+    server = Server(('', 0), RequestHandler)
+    ip, origin_port = server.server_address
+
+    # start server in thread
+    t = threading.Thread(target=server.serve_forever)
+    t.daemon = True
+    t.start()
+
+    # ConnectionLocal
+    con = ConnectionLocal()
+
+    forwarded_port = con.forward_local_port(origin_port)
+    assert connection_test("localhost", forwarded_port)
+
+    forwarded_port = con.forward_remote_port(origin_port)
+    assert connection_test("localhost", forwarded_port)
+
+    con.close_connections()
+
+    # ConnectionSSH
+    con = ConnectionSSH({"address": "localhost", "uid": "test", "password": ""})
+
+    forwarded_port = con.forward_local_port(origin_port)
+    assert connection_test("localhost", forwarded_port)
+
+    forwarded_port = con.forward_remote_port(origin_port)
+    assert connection_test("localhost", forwarded_port)
+
+    con.close_connections()
+
+    # shutdown server
+    server.shutdown()
+    server.server_close()
+
+
+LOCAL_TEST_FILES = os.path.abspath("test_files")
+REMOTE_TEST_FILES = "/home/test/test_dir/test_files"
+
+
+def test_upload_download(request):
+    def clear_test_files():
+        shutil.rmtree(LOCAL_TEST_FILES, ignore_errors=True)
+        shutil.rmtree(REMOTE_TEST_FILES, ignore_errors=True)
+    request.addfinalizer(clear_test_files)
+
+    files = ["f1.txt", "f2.txt"]
+
+    def create_dir(path):
+        shutil.rmtree(path, ignore_errors=True)
+        os.makedirs(path)
+        os.chmod(path, 0o777)
+
+    def create_files(path):
+        create_dir(path)
+        for file in files:
+            with open(os.path.join(path, file), 'w') as fd:
+                fd.write(file)
+
+    def check_files(path):
+        for file in files:
+            with open(os.path.join(path, file), 'r') as fd:
+                assert fd.read() == file
+
+    def remove_files(path):
+        shutil.rmtree(path)
+
+    # ConnectionLocal
+    con = ConnectionLocal()
+
+    loc = os.path.join(LOCAL_TEST_FILES, "loc")
+    rem = os.path.join(LOCAL_TEST_FILES, "rem")
+
+    # upload
+    create_files(loc)
+    create_dir(rem)
+    con.upload(files, loc, rem)
+    check_files(rem)
+    remove_files(loc)
+    remove_files(rem)
+
+    # download
+    create_files(rem)
+    create_dir(loc)
+    con.download(files, loc, rem)
+    check_files(loc)
+    remove_files(loc)
+    remove_files(rem)
+
+    con.close_connections()
+
+    # ConnectionSSH
+    con = ConnectionSSH({"address": "localhost", "uid": "test", "password": ""})
+
+    loc = os.path.join(LOCAL_TEST_FILES, "loc")
+    rem = os.path.join(REMOTE_TEST_FILES, "rem")
+
+    # upload
+    create_files(loc)
+    create_dir(rem)
+    con.upload(files, loc, rem)
+    check_files(rem)
+    remove_files(loc)
+    remove_files(rem)
+
+    # download
+    create_files(rem)
+    create_dir(loc)
+    con.download(files, loc, rem)
+    check_files(loc)
+    remove_files(loc)
+    remove_files(rem)
+
+    con.close_connections()
