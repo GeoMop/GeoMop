488c16c78a7f9d77bae25fee67794d3626ca6f38
Jan Brezina <jan.brezina@tul.cz>
2017-09-06 20:29:20 +0200

    Initialize effort to make standard project.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='geomop',
+      version='0.1',
+      description='Geo model preparation tools.',
+      url='http://github.com/GeoMop/GeoMop',
+      author='',
+      author_email='',
+      license='',
+      packages=[],
+      zip_safe=False)
