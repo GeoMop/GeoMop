2792197112ae50f0a71d4eb02784522582b0928e
Jan Brezina <jan.brezina@tul.cz>
2017-09-26 16:02:04 +0200

    Fix intitialize submodules script

--- tools/initialize_submodules.sh
+++ tools/initialize_submodules.sh
@@ -2,10 +2,10 @@
 
 #  Should be run from Geomop root_dir
 git submodule init
-origin_url=$$( git config --get remote.origin.url ) ;\
-if [ "$${origin_url}" != "$${origin_url#https}" ]; \
-then \
-    cp .gitmodules_https .gitmodules; \
+origin_url=`git config --get remote.origin.url` 
+if [ "${origin_url}" != "${origin_url#https}" ]
+then 
+    cp .gitmodules_https .gitmodules
 fi
 git submodule sync
 git checkout .gitmodules
