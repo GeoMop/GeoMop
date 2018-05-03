fe730d2a281324be808498a9136406293629781c
Martin Matusu <Martin.Matusu@tul.cz>
2018-05-03 16:10:49 +0200

    - get_app_icon code cleanup and exception handling  - path setup reimplementation in a singleton philosophy (singletons are not supported, modules work as singleton classes without inheritance, which is not needed in this case)  - modules resources linked in their __init__.py
    Signed-off-by: Martin Matusu <Martin.Matusu@tul.cz>

--- src/gm_base/icon.py
+++ src/gm_base/icon.py
@@ -3,40 +3,44 @@ import PyQt5.QtGui as QtGui
 import PyQt5.QtCore as QtCore
 import os
 
-__icon_dir__ = os.path.join(os.path.dirname(os.path.realpath(__file__)), "resources", "icons")
+__icon_dir__ = None
+
+
+def set_path(file):
+    global __icon_dir__
+    __icon_dir__ = os.path.join(os.path.dirname(os.path.realpath(file)), "..", "gm_base", "resources",
+                                "icons")
+
 
 def get_file(name,  size):
-    """Get file path to icon from archiv"""
-    path = __icon_dir__
-    if size == 16:
-        name += '_16'
-    elif size == 24:
-        name += '_24'
-    elif size == 32:
-        name += '_32'
-    elif size == 48:
-        name += '_48'
-    elif size == 64:
-        name += '_64'
-    elif size == 128:
-        name += '_128'
-    return os.path.join(path,name + ".png")
+    """Get file path to icon from archive"""
+    name += ('_' + str(size))
+    return os.path.join(__icon_dir__, name + ".png")
+
 
 def get_icon(name,  size):
-    """Get QIcon object from icon archiv"""
+    """Get QIcon object from icon archive"""
     return QtGui.QIcon(get_file(name,  size))
-    
+
+
 def get_app_icon(name):
-    """Get QIcon object from icon archiv"""
+    """Get QIcon object from icon archive"""
     app_icon = QtGui.QIcon()
-    app_icon.addFile(get_file(name,  16), QtCore.QSize(16,16))
-    app_icon.addFile(get_file(name,  24), QtCore.QSize(24,24))
-    app_icon.addFile(get_file(name,  32), QtCore.QSize(32,32))
-    app_icon.addFile(get_file(name,  48), QtCore.QSize(48,48))    
-    app_icon.addFile(get_file(name,  64), QtCore.QSize(64,64))
-    app_icon.addFile(get_file(name,  128), QtCore.QSize(128,128))
+    availableSizes = [16, 24, 32, 48, 64, 128]
+    try:
+        for size in availableSizes:
+            app_icon.addFile(get_file(name, size), QtCore.QSize(size, size))
+        if len(app_icon.availableSizes()) == 0:
+            raise NameError('LoadingError')
+    except NameError:
+        print(
+            "Unable to load the icon '" + str(name) + "', missing program resources. "
+            "Try checking directory: \n" + str(__icon_dir__)
+        )
+    # returns empty QIcon if loading fails and program run continues.
     return app_icon
-    
-def get_pixmap(name,  size):
-    """Get QPixmap object from icon archiv"""
+
+
+def get_pixmap(name, size):
+    """Get QPixmap object from icon archive"""
     return QtGui.QPixmap(get_file(name,  size))
