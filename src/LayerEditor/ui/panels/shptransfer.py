dde27c85ec49765bdf6d4a5a23952ed7249789cd
Martin Matusu <Martin.Matusu@tul.cz>
2018-06-28 20:08:11 +0200

    intermediate progress commit - points existence verification
    Signed-off-by: Martin Matusu <Martin.Matusu@tul.cz>

--- src/LayerEditor/ui/panels/shptransfer.py
+++ src/LayerEditor/ui/panels/shptransfer.py
@@ -19,6 +19,21 @@ class ShpTransferData():
         self.lines = []
         self.points = []
 
+    def _get_data(self):
+        polygons = []
+        lines = []
+        points = []
+        for shp in cfg.diagram.shp.datas:
+            polygons = [polygon for polygon in shp.shpdata.polygons if polygon.highlighted]
+            lines = [line for line in shp.shpdata.lines if line.highlighted]
+            points = [point.p for point in shp.shpdata.points if point.highlighted]
+        for polygon in polygons:
+            self.polygons.append(polygon)
+        for line in lines:
+            self.lines.append(line)
+        for point in points:
+            self.points.append(point)
+
     def _point_in_diagram(self, searched_point):
         '''basically for item in list overload for location comparison
         :returns list of points already in diagram that collide with searched_point
@@ -29,18 +44,28 @@ class ShpTransferData():
                 colided_points.append(point)
         return colided_points
 
+    def _simplify_polyline(self, points):
+        """Simplify a line defined by a list of points
+        :arg points list of points defining polyline [QPointF,..]
+        :returns simplified list of points [QPointF,..]"""
+        simplified = []
+        return simplified
+
     def transfer(self):
         #TODO: use line try
-        added_points = []
-        added_lines = []
-        added_polygons = []
         print("hello")
-        self.get_data()
+        self._get_data()
         print(len(self.polygons))
         print(len(self.lines))
         print(len(self.points))
+
         for point in self.points:
-            cfg.diagram._add_point(None, point)
+            points_in_diagram = self._point_in_diagram(point)
+            if not points_in_diagram:
+                cfg.diagram._add_point(None, point)
+            else:
+                print("Tried to add solitary point to a position of existing point.")
+
         for line in self.lines:
             start_points = self._point_in_diagram(line.p1)
             if not start_points:
@@ -63,7 +88,7 @@ class ShpTransferData():
             elif len(p_in_diagram) == 1:
                 cfg.main_window.diagramScene._add_line(p_in_diagram[0].object, polygon.polygon_points[0])
             else:
-                print("TODO: I found more points at point1 location, these should be merged")
+                print("TODO: I found more points at point1 location, these points should be merged")
 
             for point in polygon.polygon_points[1:]:
                 p_in_diagram = self._point_in_diagram(point)
@@ -72,7 +97,7 @@ class ShpTransferData():
                 elif len(p_in_diagram) == 1:
                     cfg.main_window.diagramScene._add_line(p_in_diagram[0].object, point, False)
                 else:
-                    print("TODO: I found more points at point1 location, these should be merged")
+                    print("TODO: I found more points at point1 location, these points should be merged")
 
             # first_point = cfg.main_window.diagramScene._last_line.p1
                 #     cfg.main_window.diagramScene._add_line(None, point, False)
@@ -94,11 +119,11 @@ class ShpTransferData():
         # cfg.main_window.diagramScene.update_changes(added_points, [], [], added_lines, [])
 
             # single polygon without checking
-            p, _ = cfg.main_window.diagramScene._add_point(None, polygon.polygon_points[0])
-            cfg.main_window.diagramScene._add_line(p, polygon.polygon_points[0])
-            for point in polygon.polygon_points[1:-1]:
-                cfg.main_window.diagramScene._add_line(None, point)
-            cfg.main_window.diagramScene._add_line(p, polygon.polygon_points[-1], False)
+            # p, _ = cfg.main_window.diagramScene._add_point(None, polygon.polygon_points[0])
+            # cfg.main_window.diagramScene._add_line(p, polygon.polygon_points[0])
+            # for point in polygon.polygon_points[1:-1]:
+            #     cfg.main_window.diagramScene._add_line(None, point)
+            # cfg.main_window.diagramScene._add_line(p, polygon.polygon_points[-1], False)
         #TODO: Creating line on another error
         #   - how does it change? Search from mouse release event
             # for polygon in polygons:
@@ -108,21 +133,6 @@ class ShpTransferData():
             # for point in cfg.diagram.shp.datas.points.highlightened:
             #     print(point.id)
             # cfg.diagram.data.datas[0].av_highlight[j]
-    def get_data(self):
-        polygons = []
-        lines = []
-        points = []
-        for shp in cfg.diagram.shp.datas:
-            polygons = [polygon for polygon in shp.shpdata.polygons if polygon.highlighted]
-            lines = [line for line in shp.shpdata.lines if line.highlighted]
-            points = [point.p for point in shp.shpdata.points if point.highlighted]
-        for polygon in polygons:
-            self.polygons.append(polygon)
-        for line in lines:
-            self.lines.append(line)
-        for point in points:
-            self.points.append(point)
-
 
 
 class ShpTransferView(QtWidgets.QWidget):
