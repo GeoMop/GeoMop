19c7db9b13b01d5a66dac38065a60e428422727d
TomasBlazek1 <tom.blazk@gmail.com>
2018-11-01 20:49:58 +0100

    Create instantiation of QActions in functions

--- src/JobPanel/ui/actions/main_menu_actions.py
+++ src/JobPanel/ui/actions/main_menu_actions.py
@@ -7,115 +7,103 @@ Actions used by main menu.
 
 import PyQt5.QtWidgets as QtWidgets
 
+def ActionExit(parent=None):
+    action = QtWidgets.QAction(parent)
+    action.setText("Exit")
+    action.setShortcut("Ctrl+Q")
+    action.setObjectName("actionExit")
+    return action
 
-class ActionExit(QtWidgets.QAction):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setText("Exit")
-        self.setShortcut("Ctrl+Q")
-        self.setObjectName("actionExit")
-
-
-class ActionLog(QtWidgets.QAction):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setText("Log")
-        self.setShortcut("Ctrl+L")
-        self.setObjectName("actionLog")
+def ActionLog(parent=None):
+    action = QtWidgets.QAction(parent)
+    action.setText("Log")
+    action.setShortcut("Ctrl+L")
+    action.setObjectName("actionLog")
+    return action
         
-class ActionRemLog(QtWidgets.QAction):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setText("Remove Log")
-        self.setShortcut("Ctrl+R")
-        self.setObjectName("actionRemLog")
+def ActionRemLog(parent=None):
+    action = QtWidgets.QAction(parent)
+    action.setText("Remove Log")
+    action.setShortcut("Ctrl+R")
+    action.setObjectName("actionRemLog")
+    return action
 
-class ActionAddMultiJob(QtWidgets.QAction):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setText("Add")
-        self.setShortcut("Alt+A")
-        self.setObjectName("actionAddMultiJob")
+def ActionAddMultiJob(parent=None):
+    action = QtWidgets.QAction(parent)
+    action.setText("Add")
+    action.setShortcut("Alt+A")
+    action.setObjectName("actionAddMultiJob")
+    return action
 
+def ActionEditMultiJob(parent=None):
+    action = QtWidgets.QAction(parent)   
+    action.setText("Edit")
+    action.setShortcut("Alt+E")
+    action.setObjectName("actionEditMultiJob")
+    return action
 
-class ActionEditMultiJob(QtWidgets.QAction):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setText("Edit")
-        self.setShortcut("Alt+E")
-        self.setObjectName("actionEditMultiJob")
+def ActionReuseMultiJob(parent=None):
+    action = QtWidgets.QAction(parent) 
+    action.setText("Reuse")
+    action.setShortcut("Alt+C")
+    action.setObjectName("actionReuseMultiJob")
+    return action
 
-
-class ActionReuseMultiJob(QtWidgets.QAction):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setText("Reuse")
-        self.setShortcut("Alt+C")
-        self.setObjectName("actionReuseMultiJob")
-
-
-class ActionDeleteMultiJob(QtWidgets.QAction):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setText("Delete MultiJob")
-        self.setObjectName("actionDeleteMultiJob")
+def ActionDeleteMultiJob(parent=None):
+    action = QtWidgets.QAction(parent)    
+    action.setText("Delete MultiJob")
+    action.setObjectName("actionDeleteMultiJob")
+    return action        
         
-        
-class ActionSendReport(QtWidgets.QAction):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setText("Create Report")
-        self.setObjectName("actionSendReport")    
+def ActionSendReport(parent=None):
+    action = QtWidgets.QAction(parent)   
+    action.setText("Create Report")
+    action.setObjectName("actionSendReport")    
+    return action
 
+def ActionDeleteRemote(parent=None):
+    action = QtWidgets.QAction(parent)    
+    action.setText("Delete Remote")
+    action.setObjectName("actionDeleteRemote")
+    return action
 
-class ActionDeleteRemote(QtWidgets.QAction):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setText("Delete Remote")
-        self.setObjectName("actionDeleteRemote")
+def ActionDownloadWholeMultiJob(parent=None):
+    action = QtWidgets.QAction(parent) 
+    action.setText("Download Whole MultiJob")
+    action.setObjectName("actionDownloadWholeMultiJob")
+    return action
 
+def ActionResumeMultiJob(parent=None):
+    action = QtWidgets.QAction(parent)    
+    action.setText("Resume")
+    action.setShortcut("Ctrl+U")
+    action.setObjectName("actionResumeMultiJob")
+    return action
 
-class ActionDownloadWholeMultiJob(QtWidgets.QAction):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setText("Download Whole MultiJob")
-        self.setObjectName("actionDownloadWholeMultiJob")
+def ActionStopMultiJob(parent=None):
+    action = QtWidgets.QAction(parent)  
+    action.setText("Stop")
+    action.setShortcut("Ctrl+S")
+    action.setObjectName("actionStopMultiJob")
+    return action
 
+def ActionSshPresets(parent=None):
+    action = QtWidgets.QAction(parent)    
+    action.setText("SSH hosts")
+    action.setShortcut("Shift+S")
+    action.setObjectName("actionSshPresets")
+    return action
 
-class ActionResumeMultiJob(QtWidgets.QAction):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setText("Resume")
-        self.setShortcut("Ctrl+U")
-        self.setObjectName("actionResumeMultiJob")
+def ActionEnvPresets(parent=None):
+    action = QtWidgets.QAction(parent)     
+    action.setText("Environments")
+    action.setShortcut("Shift+E")
+    action.setObjectName("actionEnvPresets")
+    return action
 
-
-class ActionStopMultiJob(QtWidgets.QAction):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setText("Stop")
-        self.setShortcut("Ctrl+S")
-        self.setObjectName("actionStopMultiJob")
-
-
-class ActionSshPresets(QtWidgets.QAction):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setText("SSH hosts")
-        self.setShortcut("Shift+S")
-        self.setObjectName("actionSshPresets")
-
-
-class ActionEnvPresets(QtWidgets.QAction):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setText("Environments")
-        self.setShortcut("Shift+E")
-        self.setObjectName("actionEnvPresets")
-
-class ActionOptions(QtWidgets.QAction):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setText("Set workspace")
-        self.setShortcut("Shift+O")
-        self.setObjectName("actionOptions")
+def ActionOptions(parent=None):
+    action = QtWidgets.QAction(parent)    
+    action.setText("Set workspace")
+    action.setShortcut("Shift+O")
+    action.setObjectName("actionOptions")
+    return action
