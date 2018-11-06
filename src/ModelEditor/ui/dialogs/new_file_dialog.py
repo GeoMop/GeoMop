efe9254fefebc6282f0daa0ad78665ccbe7fbf3d
Tomas Blazek <tom.blazk@gmail.com>
2018-11-06 19:31:52 +0100

    Open file name dialog when browse button clicked

--- src/ModelEditor/ui/dialogs/new_file_dialog.py
+++ src/ModelEditor/ui/dialogs/new_file_dialog.py
@@ -3,17 +3,23 @@
 .. codeauthor:: Tomas Blazek <tomas.blazek@tul.cz>
 """
 from PyQt5 import uic
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QToolButton, QPlainTextEdit
+from PyQt5 import QtGui
 
 class NewFileDialog(QDialog):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, default_path=None):
         """Initializes the class."""
         super(NewFileDialog, self).__init__(parent)
-        self = uic.loadUi("../uiDesigns/new_file_dialog.ui",self)
+        self.default_path = default_path
+        uic.loadUi("../uiDesigns/new_file_dialog.ui", self)
+        self.findChild(QToolButton,"browseButton").clicked.connect(self._open_file_browser)
 
-
-
-
+    def _open_file_browser(self):
+        location_widget = self.findChild(QPlainTextEdit,"Location")
+        new_location = QtWidgets.QFileDialog.getSaveFileName(self, "Choose location...",location_widget.toPlainText())
+        location_widget.setPlainText(new_location[0])
+        location_widget.moveCursor(QtGui.QTextCursor.End)
+        location_widget.setFocus()
 
 if __name__ == "__main__":
     import sys
