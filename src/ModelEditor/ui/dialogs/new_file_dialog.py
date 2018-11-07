cf9d2079e3a3910c707d8f2d96668bf6fb32e1f8
TomasBlazek1 <tom.blazk@gmail.com>
2018-11-07 19:48:22 +0100

    New file dialog mostly done

--- src/ModelEditor/ui/dialogs/new_file_dialog.py
+++ src/ModelEditor/ui/dialogs/new_file_dialog.py
@@ -1,31 +1,94 @@
-"""Module contains dialogs for search functions - find and replace.
+"""Dialog for creating new file with optional templates.
 
 .. codeauthor:: Tomas Blazek <tomas.blazek@tul.cz>
 """
 from PyQt5 import uic
-from PyQt5.QtWidgets import QDialog, QToolButton, QPlainTextEdit
-from PyQt5 import QtGui
+from PyQt5.QtWidgets import QDialog, QToolButton, QLineEdit, QFileDialog, QMessageBox, QGroupBox, QRadioButton,\
+                            QCheckBox
+from PyQt5.QtCore import QDir
+import os
+
 
 class NewFileDialog(QDialog):
-    def __init__(self, parent=None, default_path=None):
+    def __init__(self, parent=None, default_directory=''):
         """Initializes the class."""
         super(NewFileDialog, self).__init__(parent)
-        self.default_path = default_path
         uic.loadUi("../uiDesigns/new_file_dialog.ui", self)
-        self.findChild(QToolButton,"browseButton").clicked.connect(self._open_file_browser)
+
+        self.findChild(QLineEdit, "location").setText(default_directory)
+        self.findChild(QToolButton, "browse_button").clicked.connect(self._open_file_browser)
 
     def _open_file_browser(self):
-        location_widget = self.findChild(QPlainTextEdit,"Location")
-        new_location = QtWidgets.QFileDialog.getSaveFileName(self, "Choose location...",location_widget.toPlainText())
-        location_widget.setPlainText(new_location[0])
-        location_widget.moveCursor(QtGui.QTextCursor.End)
+        """Gets location to store new file from user"""
+        location_widget = self.findChild(QLineEdit, "location")
+        new_location = QFileDialog.getExistingDirectory(self.parent(), "Select Base Directory",
+                                                        location_widget.text(), QFileDialog.ShowDirsOnly)
+        location_widget.setText(QDir.toNativeSeparators(new_location))
         location_widget.setFocus()
+        print(self.get_file_name())
+        print(self.get_directory())
+
+    def get_directory(self):
+        """Returns location specified by user"""
+        return os.path.join(self.findChild(QLineEdit, "location").text(), "")
+
+    def get_file_name(self):
+        """Returns filename specified by user"""
+        return os.path.join(self.findChild(QLineEdit, "location").text(),
+                            self.findChild(QLineEdit, "name").text())
+
+    def templates(self):
+        """Returns a list of yaml template names"""
+        templates = ["00_header.yaml"]
+        if self.findChild(QGroupBox, "water_flow").isChecked():
+            if self.findChild(QRadioButton, "darcy").isChecked():
+                templates.append("01_flow_darcy.yaml")
+            else:
+                templates.append("01_flow_richards.yaml")
+
+        if self.findChild(QGroupBox, "solute_transport").isChecked():
+            if self.findChild(QRadioButton, "dg").isChecked():
+                templates.append("02_transport_DG.yaml")
+            else:
+                templates.append("02_transport_FV.yaml")
+
+        if self.findChild(QCheckBox, "heat_transfer").isChecked():
+            templates.append("03_heat.yaml")
+
+        return templates
+
+    def done(self, p_int):
+        """Check if provided information are correct and end dialog if they are"""
+        print(self.templates())
+        if p_int == QDialog.Rejected:
+            super().done(p_int)
+        else:
+            location = self.findChild(QLineEdit, "location").text()
+            if not self.findChild(QLineEdit, "name").text():
+                msg = QMessageBox()
+                msg.setWindowTitle("Empty Name")
+                msg.setText("Please specify name of the new file!")
+                msg.exec()
+
+            elif not location:
+                msg = QMessageBox()
+                msg.setWindowTitle("Empty Base Directory")
+                msg.setText("Please specify existing location of the new file!")
+                msg.exec()
+
+            elif not os.path.isdir(location):
+                msg = QMessageBox()
+                msg.setWindowTitle("Wrong Base Directory")
+                msg.setText("Please specify existing location of the new file!")
+                msg.exec()
+
+            else:
+                super().done(p_int)
+
 
 if __name__ == "__main__":
     import sys
     from PyQt5 import QtWidgets
     app = QtWidgets.QApplication(sys.argv)
-    NewFileWidget = QtWidgets.QDialog()
-    ui = NewFileDialog()
-    ui.show()
-    sys.exit(app.exec_())
+    ui = NewFileDialog(default_directory="C:/")
+    sys.exit(ui.exec())
