a593f05901d9ea7ea66ac5441293629e25d9ba7c
Tomas Blazek <tom.blazk@gmail.com>
2018-11-16 11:15:47 +0100

    Check if file exists and ask user if it can be overridden Changed findChild... for objects. Should be faster.

--- src/ModelEditor/ui/dialogs/new_file_dialog.py
+++ src/ModelEditor/ui/dialogs/new_file_dialog.py
@@ -3,10 +3,20 @@
 .. codeauthor:: Tomas Blazek <tomas.blazek@tul.cz>
 """
 from PyQt5 import uic
-from PyQt5.QtWidgets import QDialog, QToolButton, QLineEdit, QFileDialog, QMessageBox, QGroupBox, QRadioButton,\
-                            QCheckBox, QDialogButtonBox
+from PyQt5.QtWidgets import QDialog, QFileDialog, QMessageBox, QDialogButtonBox
 from PyQt5.QtCore import QDir
 import os
+
+"""
+location            -> QLineEdit
+name                -> QLineEdit
+browse_button       -> QDialogButtonBox
+water_flow          -> QGroupBox
+darcy               -> QRadioButton
+solute_transport    -> QGroupBox
+dg                  -> QRadioButton
+heat_transfer       -> QCheckBox
+"""
 
 
 class NewFileDialog(QDialog):
@@ -16,45 +26,43 @@ class NewFileDialog(QDialog):
         uic.loadUi(os.path.join(os.path.dirname(os.path.realpath(__file__)),'new_file_dialog.ui'), self)
 
         self.location.setText(default_directory)
-        #self.findChild(QLineEdit, "location").setText(default_directory)
-        self.findChild(QToolButton, "browse_button").clicked.connect(self._open_file_browser)
-        self.findChild(QDialogButtonBox, "button_box").button(QDialogButtonBox.Ok).setText("Create")
+        self.browse_button.clicked.connect(self._open_file_browser)
+        self.button_box.button(QDialogButtonBox.Ok).setText("Create")
 
     def _open_file_browser(self):
         """Gets location to store new file from user"""
-        location_widget = self.findChild(QLineEdit, "location")
         new_location = QFileDialog.getExistingDirectory(self.parent(), "Select Base Directory",
-                                                        location_widget.text(), QFileDialog.ShowDirsOnly)
-        location_widget.setText(QDir.toNativeSeparators(new_location))
-        location_widget.setFocus()
+                                                        self.location.text(), QFileDialog.ShowDirsOnly)
+        self.location.setText(QDir.toNativeSeparators(new_location))
+        self.location.setFocus()
 
     def get_directory(self):
         """Returns location specified by user"""
-        return os.path.join(self.findChild(QLineEdit, "location").text(), "")
+        return os.path.join(self.location.text(), "")
 
     def get_file_name(self):
         """Returns filename specified by user"""
-        name = self.findChild(QLineEdit, "name").text()
+        name = self.name.text()
         if not name.lower().endswith('.yaml'):
             name += '.yaml'
-        return os.path.join(self.findChild(QLineEdit, "location").text(), name)
+        return os.path.join(self.location.text(), name)
 
     def templates(self):
         """Returns a list of yaml template names"""
         templates = ["00_header.yaml"]
-        if self.findChild(QGroupBox, "water_flow").isChecked():
-            if self.findChild(QRadioButton, "darcy").isChecked():
+        if self.water_flow.isChecked():
+            if self.darcy.isChecked():
                 templates.append("01_flow_darcy.yaml")
             else:
                 templates.append("01_flow_richards.yaml")
 
-        if self.findChild(QGroupBox, "solute_transport").isChecked():
-            if self.findChild(QRadioButton, "dg").isChecked():
+        if self.solute_transport.isChecked():
+            if self.dg.isChecked():
                 templates.append("02_transport_DG.yaml")
             else:
                 templates.append("02_transport_FV.yaml")
 
-        if self.findChild(QCheckBox, "heat_transfer").isChecked():
+        if self.heat_transfer.isChecked():
             templates.append("03_heat.yaml")
 
         return templates
@@ -64,24 +72,31 @@ class NewFileDialog(QDialog):
         if p_int == QDialog.Rejected:
             super().done(p_int)
         else:
-            location = self.findChild(QLineEdit, "location").text()
-            if not self.findChild(QLineEdit, "name").text():
+            if not self.name.text():
                 msg = QMessageBox(self)
                 msg.setWindowTitle("Empty Name")
                 msg.setText("Please specify name of the new file!")
                 msg.exec_()
 
-            elif not location:
+            elif not self.location.text():
                 msg = QMessageBox(self)
                 msg.setWindowTitle("Empty Base Directory")
                 msg.setText("Please specify existing location of the new file!")
                 msg.exec_()
 
-            elif not os.path.isdir(location):
+            elif not os.path.isdir(self.get_directory()):
                 msg = QMessageBox(self)
                 msg.setWindowTitle("Wrong Base Directory")
                 msg.setText("Please specify existing location of the new file!")
                 msg.exec_()
+
+            elif os.path.isfile(self.get_file_name()):
+                msg = QMessageBox(self)
+                msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+                msg.setWindowTitle("File exists")
+                msg.setText("Specified file already exists. Do you wish to override it")
+                if msg.exec_() == msg.Ok:
+                    super().done(p_int)
 
             else:
                 super().done(p_int)
